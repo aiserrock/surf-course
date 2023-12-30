2fb7c2126a9f7eaa64a72d3b3008e46f997ebe52
aicserrock <aicserrock@gmail.com>
2023-12-30 23:53:09 +0300

    task-2 OOO travka muravka

--- task1/lib/surf_dart_courses_template.dart
+++ task1/lib/surf_dart_courses_template.dart
@@ -1,3 +1,193 @@
-int calculate() {
-  return 6 * 7;
+import 'package:collection/collection.dart';
+
+enum Countries { brazil, russia, turkish, spain, japan }
+
+class Territory {
+  final int areaInHectare;
+  final List<String> crops;
+  final List<AgriculturalMachinery> machineries;
+
+  Territory(
+      this.areaInHectare,
+      this.crops,
+      this.machineries,
+      );
+}
+
+class AgriculturalMachinery {
+  final String id;
+  final DateTime releaseDate;
+
+  AgriculturalMachinery(
+      this.id,
+      this.releaseDate,
+      );
+
+  /// Переопределяем оператор "==", чтобы сравнивать объекты по значению.
+  @override
+  bool operator ==(Object? other) {
+    if (other is! AgriculturalMachinery) return false;
+    if (other.id == id && other.releaseDate == releaseDate) return true;
+
+    return false;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ releaseDate.hashCode;
+}
+
+final mapBefore2010 = <Countries, List<Territory>>{
+  Countries.brazil: [
+    Territory(
+      34,
+      ['Кукуруза'],
+      [
+        AgriculturalMachinery(
+          'Трактор Степан',
+          DateTime(2001),
+        ),
+        AgriculturalMachinery(
+          'Культиватор Сережа',
+          DateTime(2007),
+        ),
+      ],
+    ),
+  ],
+  Countries.russia: [
+    Territory(
+      14,
+      ['Картофель'],
+      [
+        AgriculturalMachinery(
+          'Трактор Гена',
+          DateTime(1993),
+        ),
+        AgriculturalMachinery(
+          'Гранулятор Антон',
+          DateTime(2009),
+        ),
+      ],
+    ),
+    Territory(
+      19,
+      ['Лук'],
+      [
+        AgriculturalMachinery(
+          'Трактор Гена',
+          DateTime(1993),
+        ),
+        AgriculturalMachinery(
+          'Дробилка Маша',
+          DateTime(1990),
+        ),
+      ],
+    ),
+  ],
+  Countries.turkish: [
+    Territory(
+      43,
+      ['Хмель'],
+      [
+        AgriculturalMachinery(
+          'Комбаин Василий',
+          DateTime(1998),
+        ),
+        AgriculturalMachinery(
+          'Сепаратор Марк',
+          DateTime(2005),
+        ),
+      ],
+    ),
+  ],
+};
+
+final mapAfter2010 = {
+  Countries.turkish: [
+    Territory(
+      22,
+      ['Чай'],
+      [
+        AgriculturalMachinery(
+          'Каток Кирилл',
+          DateTime(2018),
+        ),
+        AgriculturalMachinery(
+          'Комбаин Василий',
+          DateTime(1998),
+        ),
+      ],
+    ),
+  ],
+  Countries.japan: [
+    Territory(
+      3,
+      ['Рис'],
+      [
+        AgriculturalMachinery(
+          'Гидравлический молот Лена',
+          DateTime(2014),
+        ),
+      ],
+    ),
+  ],
+  Countries.spain: [
+    Territory(
+      29,
+      ['Арбузы'],
+      [
+        AgriculturalMachinery(
+          'Мини-погрузчик Максим',
+          DateTime(2011),
+        ),
+      ],
+    ),
+    Territory(
+      11,
+      ['Табак'],
+      [
+        AgriculturalMachinery(
+          'Окучник Саша',
+          DateTime(2010),
+        ),
+      ],
+    ),
+  ],
+};
+
+void main() {
+  // Your code here.
+
+  /// №1
+  final allUniqueTechnique = <AgriculturalMachinery>{};
+  mapBefore2010.forEach(
+        (_, territoryList) {
+      for (var territory in territoryList) {
+        allUniqueTechnique.addAll(territory.machineries);
+      }
+    },
+  );
+  mapAfter2010.forEach(
+        (_, territoryList) {
+      for (var territory in territoryList) {
+        allUniqueTechnique.addAll(territory.machineries);
+      }
+    },
+  );
+
+  final machineriesList = allUniqueTechnique.toList();
+
+  final avgTechniqueAge =
+  machineriesList.map((e) => e.releaseDate.year).average.round();
+  print('Средний возраст всей техники на всех угодьях: $avgTechniqueAge');
+
+  /// №2
+  machineriesList.sort((a, b) => a.releaseDate.compareTo(b.releaseDate));
+  final fifteenPercentOfAllTechnique = machineriesList.sublist(
+    0,
+    (machineriesList.length / 2).round(),
+  );
+  final avgFifteenPercentOfAllTechnique =
+  fifteenPercentOfAllTechnique.map((e) => e.releaseDate.year).average.round();
+  print(
+      '50% самой старой техники, её средний возраст $avgFifteenPercentOfAllTechnique');
 }
