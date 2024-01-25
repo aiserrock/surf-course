55bd81ea7bfe1ae84c905eca189837db2a2572a9
aiserrock <aiserrock@gmail.com>
2024-01-25 14:29:54 +0300

    rm unused files

--- task2/bin/surf_dart_courses_template.dart
+++
@@ -1,5 +0,0 @@
-import 'package:surf_dart_courses_template/main.dart' as surf_dart_courses_template;
-
-void main(List<String> arguments) {
-  print('Hello world: ${surf_dart_courses_template.calculate()}!');
-}
--- task2/test/surf_dart_courses_template_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:surf_dart_courses_template/main.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
